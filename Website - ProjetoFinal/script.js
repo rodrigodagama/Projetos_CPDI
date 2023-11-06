b795bebff11d3bf5381f668dea6e83a7fb115801
rodrigodagama <rodrigodagama@outlook.com>
2023-11-06 09:50:25 -0300

    new

--- Website - ProjetoFinal/script.js
+++ Website - ProjetoFinal/script.js
@@ -1,9 +1,8 @@
+const toggleBtn = document.querySelector('.navbar-toggle');
+const navbarLinks = document.querySelectorAll('.navbar-links');
 
-const toggleBtn = document.getElementsByClassName('navbar-toggle')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links');
-
-toggleBtn.addEventListener('click', function(){
-    for(let i=0; i<navbarLinks.length; i++){
+toggleBtn.addEventListener('click', function () {
+    for (let i = 0; i < navbarLinks.length; i++) {
         navbarLinks[i].classList.toggle('active');
     }
 });
